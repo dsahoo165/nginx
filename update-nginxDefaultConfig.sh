3cad95d99cc46591ddc43be1c4c627414f2d2e00
dsahoo165 <44152066+dsahoo165@users.noreply.github.com>
2023-06-13 19:02:13 +0530

    Update update-nginxDefaultConfig.sh

--- update-nginxDefaultConfig.sh
+++ update-nginxDefaultConfig.sh
@@ -1,3 +1,3 @@
-docker cp ${env.WORKSPACE}/default.conf nginx-container:/etc/nginx/conf.d/
+docker cp default.conf nginx-container:/etc/nginx/conf.d/
 docker exec nginx-container nginx -t
 docker exec nginx-container nginx -s reload
